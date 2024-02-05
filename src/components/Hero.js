d80124168123f1f79699ac0000104a5a24a2e8ee
MoizShah-227 <103181576+MoizShah-227@users.noreply.github.com>
2024-02-05 23:43:49 +0500

    header done

---
+++ src/components/Hero.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Hero.css'
+import rightimg from '../img/Rightside.svg'
+const Hero = () => {
+  return (
+    <div className='hero-main mt-3'>
+      <div className='container'>
+        <div className='row'> 
+            <div className='left col-lg-6 mt-5 pt-5'>
+                <h1>We Develop Highly</h1>
+                <h1><span>Efficient </span>And</h1>
+                <h1>Amazing Websites!</h1>
+                <button className=' mt-2 mx-2 btn'>My Resume</button>
+            </div>
+            <div className='right col-lg-6 '>
+            <img className='right-side-img' src={rightimg}/>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
